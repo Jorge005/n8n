38e1dd0d8c74eb770593c53b65f2449e2d888800
Jorge005 <jorge005@hotmail.com>
2026-04-19 13:26:19 -0500

    Update Logo.vue

--- packages/frontend/@n8n/design-system/src/components/N8nLogo/Logo.vue
+++ packages/frontend/@n8n/design-system/src/components/N8nLogo/Logo.vue
@@ -59,7 +59,6 @@ onMounted(() => {
 <template>
 	<div :class="containerClasses" data-test-id="n8n-logo">
 		<LogoIcon ref="logo" :class="$style.logo" />
-		// <LogoText v-if="showLogoText" :class="$style.logoText" />
 		<slot />
 	</div>
 </template>
